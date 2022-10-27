83748655062038aac1246f7107fc65ce286d2ca3
marlisonmourao <bentesmourao@gmail.com>
2022-10-27 05:31:30 -0400

    feat: signin screen

--- App.tsx
+++ App.tsx
@@ -2,16 +2,22 @@ import { View, StatusBar } from "react-native";
 import { NativeBaseProvider } from "native-base"
 import { useFonts,Roboto_400Regular,Roboto_700Bold } from "@expo-google-fonts/roboto";
 
+import { THEME } from "./src/theme";
+
+import { Loading } from "@components/Loading";
+
+import { Signin } from "@screens/Signin";
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <View/>: <View />}
+      {fontsLoaded ? <Signin/>: <Loading />}
     </NativeBaseProvider>
   );
 }
