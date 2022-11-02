9b34b5de4510d32dda90df8eb0c9e3720de8c052
marlisonmourao <bentesmourao@gmail.com>
2022-11-02 04:24:59 -0400

    finalizing screens

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react"
+import { useNavigation } from "@react-navigation/native";
 import { HStack, VStack, FlatList, Heading, Text } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 import { ExerciseCard } from "@components/ExerciseCard";
 
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
 export function Home() {
   const [groups, setGroups] = useState(['costa', 'ombro', 'perna', 'braços']);
   const [exercises, setExercices] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terra']);
   const [groupSelected, setGroupSelected] = useState('costa');
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercise");
+  }
 
   return(
     <VStack flex={1}> 
@@ -48,7 +57,10 @@ export function Home() {
             data={exercises}
             keyExtractor={item => item}
             renderItem={({ item }) => (
-              <ExerciseCard title={item}  />
+              <ExerciseCard 
+                title={item}  
+                onPress={handleOpenExerciseDetails}
+              />
             )}
 
             showsVerticalScrollIndicator={false}
